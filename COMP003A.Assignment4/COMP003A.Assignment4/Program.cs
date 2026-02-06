3a711f72639d608afa6af98f482181773b184907
j26ronquillo <joshalvinpogi26@yahoo.com>
2026-02-05 17:30:27 -0800

    created the menu for displaya nd made a draft on the routine progress tracking

---
+++ COMP003A.Assignment4/COMP003A.Assignment4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Design;
+
+namespace COMP003A.Assignment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Start/Continue Routine\n2. View Progress\n3. Exit\n");
+
+            bool option = false;
+            int stepNumber = 0;
+            while (option == false)
+            { Console.Write("Enter Choice: ");
+            int userInput = int.Parse(Console.ReadLine());
+                if (userInput == 1) ;
+                { Console.WriteLine("Routine step 1 complete.\n"); };
+                else if (userInput == 2) ;
+				{ Console.WriteLine($"Current progress: {stepNumber} steps completed"); };
+               
+			}
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
