1ee73007c37fc35e1cd2f92fab1b7bebc6b7b602
j26ronquillo <joshalvinpogi26@yahoo.com>
2026-02-05 18:29:05 -0800

    answered a couple comment requirements

--- COMP003A.Assignment4/COMP003A.Assignment4/Program.cs
+++ COMP003A.Assignment4/COMP003A.Assignment4/Program.cs
@@ -12,8 +12,9 @@ namespace COMP003A.Assignment4
             bool option = false;
             int currentNumber = 1;
             int stepNumber = 0;
-
-            while (option == false)
+            /* Loops exist because it helps the programmer help with continuous displays instead of them having to copy mutliple blocks.
+             It also executes a block of code repeatly until a specific condition is met.*/
+            while (option == false) 
             {
                 Console.Write("Enter Choice: ");
                 int userInput = int.Parse(Console.ReadLine());
@@ -29,7 +30,8 @@ namespace COMP003A.Assignment4
 
                 else if (userInput == 3)
                 { Console.WriteLine("Program ended."); break; }
-             
+             /* Loops skip steps or blocks because of control flow statements like using continue or break. If the condition for the value is not true to begin with,
+              * the program would never start. Managing control flow in loop statements must be watched carefully as it can accidently lead to infinite loops if not used.*/
             
             }
         }
