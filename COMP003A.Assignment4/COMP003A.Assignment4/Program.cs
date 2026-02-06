9e9e8b87a3d6df5651192708c5d3cd3e15fe05f7
j26ronquillo <joshalvinpogi26@yahoo.com>
2026-02-05 18:21:47 -0800

    messed up the format of "userInput = 1" and then fixed it. also, changed the program ending command to "break"

--- COMP003A.Assignment4/COMP003A.Assignment4/Program.cs
+++ COMP003A.Assignment4/COMP003A.Assignment4/Program.cs
@@ -14,18 +14,23 @@ namespace COMP003A.Assignment4
             int stepNumber = 0;
 
             while (option == false)
-            { Console.Write("Enter Choice: ");
+            {
+                Console.Write("Enter Choice: ");
                 int userInput = int.Parse(Console.ReadLine());
-				if (userInput == 1)
+                if (userInput == 1)
                 {
-					if (currentNumber == 3) { Console.WriteLine("Step 3 is restricted and was skipped.\n"); stepNumber++; currentNumber++; continue; }
-					if (currentNumber == 5) { Console.WriteLine("Safety limit reach. Routine stopped.\n"); continue; }
-					Console.WriteLine($"Routine {currentNumber} complete.\n"); stepNumber++; currentNumber++;}
+                    if (currentNumber == 3) { Console.WriteLine("Step 3 is restricted and was skipped.\n"); stepNumber++; currentNumber++; continue; }
+                    if (currentNumber == 5) { Console.WriteLine("Safety limit reach. Routine stopped.\n"); continue; }
+                    Console.WriteLine($"Routine {currentNumber} complete.\n"); stepNumber++; currentNumber++;
+                }
+
                 else if (userInput == 2)
                 { Console.WriteLine($"Current progress: {stepNumber} steps completed.\n"); }
-                else if (userInput == 3)
-                { Console.WriteLine("Program ended."); option = true; }
 
+                else if (userInput == 3)
+                { Console.WriteLine("Program ended."); break; }
+             
+            
             }
         }
     }
