02e4e9002fe9a84a06f31424218847cece5ae9bb
j26ronquillo <joshalvinpogi26@yahoo.com>
2026-02-05 17:49:36 -0800

    finished the routine progress tracking, routine step is completed, can indicate the competed step, and stops when option 3 is chosen.

--- COMP003A.Assignment4/COMP003A.Assignment4/Program.cs
+++ COMP003A.Assignment4/COMP003A.Assignment4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.Design;
+using System.Diagnostics;
 
 namespace COMP003A.Assignment4
 {
@@ -9,14 +10,18 @@ namespace COMP003A.Assignment4
             Console.WriteLine("1. Start/Continue Routine\n2. View Progress\n3. Exit\n");
 
             bool option = false;
+            int currentNumber = 1;
             int stepNumber = 0;
+           
             while (option == false)
             { Console.Write("Enter Choice: ");
             int userInput = int.Parse(Console.ReadLine());
-                if (userInput == 1) ;
-                { Console.WriteLine("Routine step 1 complete.\n"); };
-                else if (userInput == 2) ;
-				{ Console.WriteLine($"Current progress: {stepNumber} steps completed"); };
+                if (userInput == 1) 
+                { Console.WriteLine($"Routine {currentNumber} complete.\n"); stepNumber++; currentNumber++; }
+                if (userInput == 2) 
+				{ Console.WriteLine($"Current progress: {stepNumber} steps completed.\n"); }
+                if (userInput == 3) 
+				{ Console.WriteLine("Program ended."); option = true; }
                
 			}
 
